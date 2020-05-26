4718cc846ebc24fa7775bf32c6205c96522140ba
Rohit Tayal <tayal.rohit14@gmail.com>
2020-05-26 12:49:18 +0530

    adding FileTree component

--- src/Components/fileTree/index.js
+++ src/Components/fileTree/index.js
@@ -1,30 +1,33 @@
 import React, { useEffect, useState } from "react";
+import generateFileTreeObject from "./utils";
 
-const FileTree = props => {
-  const [state, setState] = useState(props.files || [])
-  useEffect() {
+const FileTree = (props) => {
+  const [state, setState] = useState(props.files || []);
+  useEffect(() => {
     generateFileTreeObject(props.directory)
-      .then(files => {
-        setState({ files })
+      .then((files) => {
+        setState({ files });
       })
-      .catch(console.error)
-  }
-    const files = state.files;
-    return (
-      <ul>
-        {files && files.map(file => {
+      .catch(console.error);
+  });
+  const files = state.files;
+  return (
+    <ul>
+      {files &&
+        files.map((file) => {
           const filePath = file.filePath;
-          const fileName = filePath.split('/').slice(-1).join('');
-          return file.isFileBool ?
-            <li key={filePath + ' Directory'}>{`${fileName}`}
-              <FileTree directory={file.filePath} files={file.files} />}
+          const fileName = filePath.split("/").slice(-1).join("");
+          return file.isFileBool ? (
+            <li key={filePath + " Directory"}>
+              {`${fileName}`}
+              <FileTree directory={file.filePath} files={file.files} />
             </li>
-            :
-            <li key={filePath}>{`${fileName}`}</li>;
-          })
-        }
-      </ul>
-    )
+          ) : (
+            <li key={filePath}>{`${fileName}`}</li>
+          );
+        })}
+    </ul>
+  );
 };
 
 export default FileTree;
